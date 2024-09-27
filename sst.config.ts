f542cc30187970f4e7c704b36d1bffb2ad7c3b3a
jessekerch <jesse.kercheval@gmail.com>
2024-09-27 16:14:31 -0700

    Initial commit

--- sst.config.ts
+++ sst.config.ts
@@ -1,19 +1,15 @@
-/// <reference path="./.sst/platform/config.d.ts" />
+/// <reference path=".sst/platform/config.d.ts" />
 
 export default $config({
   app(input) {
     return {
-      name: "monorepo-template",
+      name: "sst-notes",
       removal: input?.stage === "production" ? "retain" : "remove",
       home: "aws",
     };
   },
   async run() {
     await import("./infra/storage");
-    const api = await import("./infra/api");
-
-    return {
-      api: api.myApi.url,
-    };
+    await import("./infra/api");
   },
 });
